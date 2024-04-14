ff9f83c06b642204f098ce73460bd853ce70583e
andrewstech <andrew@win11react.com>
2024-04-14 17:09:28 +0100

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -5,30 +5,39 @@ const directoryPath = path.join(__dirname, "../../domains");
 
 let combinedArray = [];
 
-function readFiles(dir) {
-    const files = fs.readdirSync(dir);
-    files.forEach(file => {
-        const filePath = path.join(dir, file);
-        const stats = fs.statSync(filePath);
-        if (stats.isDirectory()) {
-            // Recursively read files in subdirectories
-            readFiles(filePath); 
-        } else {
-            // Check if the file is not from the "reserved" directory
-            if (!filePath.includes(path.join("domains", "reserved"))) {
-                const data = fs.readFileSync(filePath, "utf8");
-                const dataArray = [JSON.parse(data)];
-                for (const item of dataArray) {
-                    if (item.owner && item.owner.email) {
-                        item.owner.email = item.owner.email.replace(/@/, " (at) ");
-                    }
-                }
-                combinedArray = combinedArray.concat(dataArray);
-            }
+fs.readdir(directoryPath, function (err, files) {
+    if (err) throw err;
+
+    function removeValue(value, index, arr) {
+        if (value === "reserved") {
+            arr.splice(index, 1);
+            return true;
         }
+
+        return false;
+    }
+
+    files.filter(removeValue);
+
+    files.forEach(function (file) {
+        const filePath = path.join(directoryPath, file);
+
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) throw err;
+
+            const dataArray = [JSON.parse(data)];
+
+            for (const item of dataArray) {
+                item.owner.email = item.owner.email.replace(/@/, " (at) ");
+            }
+
+            combinedArray = combinedArray.concat(dataArray);
+
+            if (combinedArray.length === files.length) {
+                fs.writeFile("raw/index.json", JSON.stringify(combinedArray), (err) => {
+                    if (err) throw err;
+                });
+            }
+        });
     });
-}
-
-readFiles(directoryPath);
-
-fs.writeFileSync("raw/index.json", JSON.stringify(combinedArray));
+});
